96a89a7ef736b2c3d506ba7afe5cf2d76cd8fd71
andrewchen <andrew.chen923@gmail.com>
2018-10-13 15:30:40 -0400

    finished basic requirement

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
-import { renderToString } from "react-dom/server";
+import showdown from "showdown";
+import { Link } from "react-router-dom";
 import "../styles/navbar.scss";
+
+const converter = new showdown.Converter();
 
 export default class Navbar extends Component {
   constructor(props) {
@@ -18,22 +21,34 @@ export default class Navbar extends Component {
   }
 
   download(format) {
+    // choose to compile to html or leave the string as input to blob
+    const blobInput =
+      format === "src" ? this.props.code : converter.makeHtml(this.props.code);
+
     // create blob from code
-    const blob = new Blob([this.props.code])
-    const blobURL = URL.createObjectURL(blob)
-    const extension = (format === 'src') ? "md" : "html"
-    const filename = `${this.state.title.replace(/ /g, "_")}.${extension}`
+    const blob = new Blob([blobInput]);
+    const blobURL = URL.createObjectURL(blob);
+
+    // create the new filename
+    const extension = format === "src" ? "md" : "html";
+    const filename = `${this.state.title.replace(/ /g, "_")}.${extension}`;
+
     // create a link and click it
-    let link = document.createElement('a');
-    link.href = blobURL
-    link.download = filename
-    link.click()
+    let link = document.createElement("a");
+    link.href = blobURL;
+    link.download = filename;
+    link.click();
   }
 
   render() {
     return (
       <div className="navbar">
-        <input value={this.state.title} onChange={e => this.onTitleChange(e)} />
+        <input
+          className="filename"
+          placeholder="Enter Filename..."
+          value={this.state.title}
+          onChange={e => this.onTitleChange(e)}
+        />
 
         {/* <select name="keybindings" id="keybinds" onChange={e => this.props.keybindingsOnChange(e)}>
           <option default value="">Normal</option>
@@ -41,8 +56,13 @@ export default class Navbar extends Component {
           <option value="emacs">Emacs</option>
         </select> */}
 
-        <button onClick={() => this.download('src')}>Download Markdown</button>
-        <button onClick={() => this.download('html')}>Download HTML</button>
+        <div className="buttons">
+          <button onClick={() => this.download("src")}>
+            Download Markdown
+          </button>
+          <button onClick={() => this.download("html")}>Download HTML</button>
+        </div>
+        <Link to="/">Back to Home</Link>
       </div>
     );
   }
